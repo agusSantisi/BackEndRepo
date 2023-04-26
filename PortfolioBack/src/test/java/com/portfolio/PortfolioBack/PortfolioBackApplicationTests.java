824cd3749607c236873b7e13e81a9ef7e42c3ce9
agusSantisi <agustinargprograma@gmail.com>
2023-04-26 11:00:49 -0300

    Initial commit

---
+++ PortfolioBack/src/test/java/com/portfolio/PortfolioBack/PortfolioBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.PortfolioBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
